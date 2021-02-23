44e4cd8a9fa2cef40ba02b3413a424257e679c0f
Adam Kowalski <adam.kowalski.work@gmail.com>
2021-02-22 22:30:50 -0800

    binary operators for signed integers

--- tests/codegen.zig
+++ tests/codegen.zig
@@ -6,7 +6,7 @@ const lang = @import("lang");
 const parse = lang.parse;
 const lower = lang.lower;
 
-test "add two constants" {
+test "add two signed integers" {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer std.testing.expect(!gpa.deinit());
     const allocator = &gpa.allocator;
@@ -33,12 +33,12 @@ test "add two constants" {
         \\    section .text
         \\
         \\_main:
-        \\    call main
+        \\    call label0
         \\    mov rdi, rax
         \\    mov rax, 33554433
         \\    syscall
         \\
-        \\main:
+        \\label0:
         \\    push rbp
         \\    mov rbp, rsp
         \\    mov rax, 10
@@ -49,7 +49,7 @@ test "add two constants" {
     );
 }
 
-test "add three constants" {
+test "add three signed integers" {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer std.testing.expect(!gpa.deinit());
     const allocator = &gpa.allocator;
@@ -77,12 +77,12 @@ test "add three constants" {
         \\    section .text
         \\
         \\_main:
-        \\    call main
+        \\    call label0
         \\    mov rdi, rax
         \\    mov rax, 33554433
         \\    syscall
         \\
-        \\main:
+        \\label0:
         \\    push rbp
         \\    mov rbp, rsp
         \\    mov rax, 10
@@ -90,6 +90,239 @@ test "add three constants" {
         \\    add rax, rbx
         \\    mov rcx, 30
         \\    add rax, rcx
+        \\    pop rbp
+        \\    ret
+    );
+}
+
+test "subtract two signed integers" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.testing.expect(!gpa.deinit());
+    const allocator = &gpa.allocator;
+    const source =
+        \\(fn main :args () :ret i64
+        \\  :body
+        \\  (const x 10)
+        \\  (const y 15)
+        \\  (- x y))
+    ;
+    var interned_strings = try lang.data.interned_strings.prime(&gpa.allocator);
+    defer interned_strings.deinit();
+    var ast = try lang.parse(&gpa.allocator, &interned_strings, source);
+    defer ast.deinit();
+    var ir = try lang.lower(&gpa.allocator, ast);
+    defer ir.deinit();
+    var x86 = try lang.codegen(allocator, ir, interned_strings);
+    defer x86.deinit();
+    var x86_string = try lang.x86String(allocator, x86, interned_strings);
+    defer x86_string.deinit();
+    std.testing.expectEqualStrings(x86_string.slice(),
+        \\    global _main
+        \\
+        \\    section .text
+        \\
+        \\_main:
+        \\    call label0
+        \\    mov rdi, rax
+        \\    mov rax, 33554433
+        \\    syscall
+        \\
+        \\label0:
+        \\    push rbp
+        \\    mov rbp, rsp
+        \\    mov rax, 10
+        \\    mov rbx, 15
+        \\    sub rax, rbx
+        \\    pop rbp
+        \\    ret
+    );
+}
+
+test "multiply two signed integers" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.testing.expect(!gpa.deinit());
+    const allocator = &gpa.allocator;
+    const source =
+        \\(fn main :args () :ret i64
+        \\  :body
+        \\  (const x 10)
+        \\  (const y 15)
+        \\  (* x y))
+    ;
+    var interned_strings = try lang.data.interned_strings.prime(&gpa.allocator);
+    defer interned_strings.deinit();
+    var ast = try lang.parse(&gpa.allocator, &interned_strings, source);
+    defer ast.deinit();
+    var ir = try lang.lower(&gpa.allocator, ast);
+    defer ir.deinit();
+    var x86 = try lang.codegen(allocator, ir, interned_strings);
+    defer x86.deinit();
+    var x86_string = try lang.x86String(allocator, x86, interned_strings);
+    defer x86_string.deinit();
+    std.testing.expectEqualStrings(x86_string.slice(),
+        \\    global _main
+        \\
+        \\    section .text
+        \\
+        \\_main:
+        \\    call label0
+        \\    mov rdi, rax
+        \\    mov rax, 33554433
+        \\    syscall
+        \\
+        \\label0:
+        \\    push rbp
+        \\    mov rbp, rsp
+        \\    mov rax, 10
+        \\    mov rbx, 15
+        \\    imul rax, rbx
+        \\    pop rbp
+        \\    ret
+    );
+}
+
+test "divide two signed integers" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.testing.expect(!gpa.deinit());
+    const allocator = &gpa.allocator;
+    const source =
+        \\(fn main :args () :ret i64
+        \\  :body
+        \\  (const x 20)
+        \\  (const y 4)
+        \\  (/ x y))
+    ;
+    var interned_strings = try lang.data.interned_strings.prime(&gpa.allocator);
+    defer interned_strings.deinit();
+    var ast = try lang.parse(&gpa.allocator, &interned_strings, source);
+    defer ast.deinit();
+    var ir = try lang.lower(&gpa.allocator, ast);
+    defer ir.deinit();
+    var x86 = try lang.codegen(allocator, ir, interned_strings);
+    defer x86.deinit();
+    var x86_string = try lang.x86String(allocator, x86, interned_strings);
+    defer x86_string.deinit();
+    std.testing.expectEqualStrings(x86_string.slice(),
+        \\    global _main
+        \\
+        \\    section .text
+        \\
+        \\_main:
+        \\    call label0
+        \\    mov rdi, rax
+        \\    mov rax, 33554433
+        \\    syscall
+        \\
+        \\label0:
+        \\    push rbp
+        \\    mov rbp, rsp
+        \\    mov rax, 20
+        \\    mov rbx, 4
+        \\    cqo
+        \\    idiv rbx
+        \\    pop rbp
+        \\    ret
+    );
+}
+
+test "divide two signed integers where lhs is not in rax" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.testing.expect(!gpa.deinit());
+    const allocator = &gpa.allocator;
+    const source =
+        \\(fn main :args () :ret i64
+        \\  :body
+        \\  (const a 2)
+        \\  (const b 3)
+        \\  (const c (+ a b))
+        \\  (const d 30)
+        \\  (/ d c))
+    ;
+    var interned_strings = try lang.data.interned_strings.prime(&gpa.allocator);
+    defer interned_strings.deinit();
+    var ast = try lang.parse(&gpa.allocator, &interned_strings, source);
+    defer ast.deinit();
+    var ir = try lang.lower(&gpa.allocator, ast);
+    defer ir.deinit();
+    var x86 = try lang.codegen(allocator, ir, interned_strings);
+    defer x86.deinit();
+    var x86_string = try lang.x86String(allocator, x86, interned_strings);
+    defer x86_string.deinit();
+    std.testing.expectEqualStrings(x86_string.slice(),
+        \\    global _main
+        \\
+        \\    section .text
+        \\
+        \\_main:
+        \\    call label0
+        \\    mov rdi, rax
+        \\    mov rax, 33554433
+        \\    syscall
+        \\
+        \\label0:
+        \\    push rbp
+        \\    mov rbp, rsp
+        \\    mov rax, 2
+        \\    mov rbx, 3
+        \\    add rax, rbx
+        \\    mov rcx, rax
+        \\    mov rax, 30
+        \\    cqo
+        \\    idiv rcx
+        \\    pop rbp
+        \\    ret
+    );
+}
+
+test "binary operators on signed integers" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.testing.expect(!gpa.deinit());
+    const allocator = &gpa.allocator;
+    const source =
+        \\(fn main :args () :ret i64
+        \\  :body
+        \\  (const a 10)
+        \\  (const b 7)
+        \\  (const c 3)
+        \\  (const d 15)
+        \\  (const e 2)
+        \\  (/ (+ (* (- a b) c) d) e))
+    ;
+    var interned_strings = try lang.data.interned_strings.prime(&gpa.allocator);
+    defer interned_strings.deinit();
+    var ast = try lang.parse(&gpa.allocator, &interned_strings, source);
+    defer ast.deinit();
+    var ir = try lang.lower(&gpa.allocator, ast);
+    defer ir.deinit();
+    var x86 = try lang.codegen(allocator, ir, interned_strings);
+    defer x86.deinit();
+    var x86_string = try lang.x86String(allocator, x86, interned_strings);
+    defer x86_string.deinit();
+    std.testing.expectEqualStrings(x86_string.slice(),
+        \\    global _main
+        \\
+        \\    section .text
+        \\
+        \\_main:
+        \\    call label0
+        \\    mov rdi, rax
+        \\    mov rax, 33554433
+        \\    syscall
+        \\
+        \\label0:
+        \\    push rbp
+        \\    mov rbp, rsp
+        \\    mov rax, 10
+        \\    mov rbx, 7
+        \\    sub rax, rbx
+        \\    mov rcx, 3
+        \\    imul rax, rcx
+        \\    mov rdx, 15
+        \\    add rax, rdx
+        \\    mov rsi, 2
+        \\    mov rdi, rdx
+        \\    cqo
+        \\    idiv rsi
         \\    pop rbp
         \\    ret
     );
